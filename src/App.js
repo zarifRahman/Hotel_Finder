db165db578007f02d5bbbfe4f623b6d78e3c0393
zarifRahman <zarif.shahriar.rahman@gmail.com>
2022-01-20 01:32:45 +0600

    Google map initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './Header/Header';
+import List from './List/List';
+import Map from './Map/Map';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map/>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlaceDetail = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PlaceDetail;
